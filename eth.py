49f34d3f57d5713ac1067052ec37bd204e5abd90
Marcus K <133644816+MarcusProgram@users.noreply.github.com>
2023-09-17 10:46:29 +0300

    Add files via upload

--- eth.py
+++ eth.py
@@ -1,8 +1,9 @@
 import blocksmith
+from web3 import Web3, HTTPProvider
 from eth_keys import keys
 from eth_utils import decode_hex
-import pyetherbalance
 from colorama import Fore, Back, init
+from time import sleep
 # Все импорты
 init()
 
@@ -25,18 +26,27 @@ def generate_public_key(private_key):
     pub_key = priv_key.public_key
     return pub_key
 def check_balance_print(address,key):
-    infura_url = 'https://mainnet.infura.io/v3/eec9b42b403f4a3aad1099940d4abe36'
-    ethbalance = pyetherbalance.PyEtherBalance(infura_url)
-    balance_eth = ethbalance.get_eth_balance(address)
-    if balance_eth['balance'] > 0.000000000001:
-        with open("goods.txt", "w+") as my_file:
-            my_file.write(f"Address: {address};"
-                          f" Public Key: {generate_public_key(key)};"
-                          f" Private Key: {key};"
-                          f" ETH:{balance_eth['balance']}")
-        print(f"{Back.YELLOW}{Fore.WHITE}Address: {Fore.CYAN}{address} {Fore.GREEN}| {Fore.RED}ETH: {balance_eth['balance']}")  # Если есть eth то делает текст подствеченным
-    else:
-        print(f"{Fore.WHITE}Address: {Fore.CYAN}{address} {Fore.GREEN} | {Fore.RED}ETH: {balance_eth['balance']}")  # а если нету то такой какой есть цвет
+    try:
+        provider = HTTPProvider('https://mainnet.infura.io/v3/eec9b42b403f4a3aad1099940d4abe36')
+        if (provider.is_connected()):
+            web3 = Web3(provider)
+            address = Web3.to_checksum_address(address)
+            balance_wei = web3.eth.get_balance(address)
+            balance_eth = web3.from_wei(balance_wei, 'ether')
+            if balance_eth> 0.000000000001:
+                with open("goods.txt", "w+") as my_file:
+                    my_file.write(f"Address: {address};"
+                                  f" Public Key: {generate_public_key(key)};"
+                                  f" Private Key: {key};"
+                                  f" ETH:{balance_eth}")
+                print(f"{Back.YELLOW}{Fore.WHITE}Address: {Fore.CYAN}{address} {Fore.GREEN}| {Fore.RED}ETH: {balance_eth}")  # Если есть eth то делает текст подствеченным
+            else:
+                print(f"{Fore.WHITE}Address: {Fore.CYAN}{address} {Fore.GREEN} | {Fore.RED}ETH: {balance_eth}")  # а если нету то такой какой есть цвет
+        else:
+            sleep(80)
+            start(0)
+    except Exception as e:
+        print(e)
 
 
 def generate_address(key):
@@ -52,9 +62,11 @@ def generate_private_key():
     generate_public_key(key)
     return key
 
-def start():
-    i = 0 # Подсчет колво проверок
+def start(i):
     while True: #Вечный цикл
         print(f"[{i}]", generate_private_key()) #Выводин на экран и делает выше функцию
         i += 1  # прибавляет к i + 1
-start()
+        #if (i % 50 == 0):
+        #    sleep(90)
+
+start(0)
